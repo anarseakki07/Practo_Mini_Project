abf991dc27cb3812f85e6817c76bea621b0d1dc7
Akshay Anarse <anarseakki@gmail.com>
2021-09-24 10:08:15 +0530

    update

--- src/main/java/com/data/Practo.java
+++ src/main/java/com/data/Practo.java
@@ -2,16 +2,9 @@ package com.data;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
