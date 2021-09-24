a1eaa717c94355eff10c79185b68cb9ff68e98a5
Akshay Anarse <anarseakki@gmail.com>
2021-09-24 10:06:03 +0530

    Update

--- src/main/java/com/data/Practo.java
+++ src/main/java/com/data/Practo.java
@@ -1,16 +1,18 @@
 package com.data;
 import java.awt.AWTException;
-//import java.awt.Robot;
-//import java.awt.event.KeyEvent;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -26,41 +28,66 @@ public class Practo extends Read
 
 	public static void main(String[] args) throws IOException, InterruptedException, AWTException 
 	{
-		System.setProperty("webdriver.chrome.driver", 
-				"C:\\Users\\91959\\eclipse-workspace\\MiniProject\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\91959\\eclipse-workspace\\MiniProject\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		// To Maximize The Window
 		driver.manage().window().maximize();
-		File f1 = new File("C:\\Users\\91959\\eclipse-workspace\\MiniProject\\txtfolder");
+		
+		// To Create Folder 
+		File f1 = new File("C:\\Users\\91959\\eclipse-workspace\\MiniProject\\txtFolder");
 		boolean b = f1.mkdir();
 		System.out.println("Folder Created Successfully."+b);
-		File f2 = new File("C:\\\\Users\\\\91959\\\\eclipse-workspace\\\\MiniProject\\\\txtfolder\\\\write.txt");
-		boolean c = f2.createNewFile();
-		System.out.println("File Created Successfully."+c);
+		
+		// To Create File in Folder
+		File xp = new File("C:\\\\Users\\\\91959\\\\eclipse-workspace\\\\MiniProject\\\\txtFolder\\\\Cities.txt");
+		boolean v = xp.createNewFile();
+		System.out.println("File Created uccessfully."+v);
+		
+		// Actions and Robot is Imported For Key Press event and Click Event
 		Actions Acc = new Actions(driver);
 		Robot r = new Robot();
+		
+		// Data is Retrived From Read.java Class 
 		String[] rw = getData();
+		
+		
 		for(int i=0;i<=2;i++)
 		{
-			
+			// Go to URL 
 			driver.get("https://www.practo.com/");
 			Thread.sleep(2000);
+			
+			// Double Click on Location Field
 			WebElement mou1 = driver.findElement(By.xpath("//*[@id=\"c-omni-container\"]/div/div[1]/div[1]/input"));
 			Acc.doubleClick(mou1).build().perform();
+			
+			// Entering Data in Location Field
 			WebElement location = driver.findElement(By.xpath("//*[@id=\"c-omni-container\"]/div/div[1]/div[1]/input"));
-			location.sendKeys(rw[i]);
+			location.sendKeys(rw[i]);		
 			Thread.sleep(2000);
+			
+			// Entering Data in Next Field as a type
 			WebElement hos = driver.findElement(By.xpath("//*[@id=\"c-omni-container\"]/div/div[2]/div[1]/input"));
 			hos.sendKeys("Hospital");
 			Thread.sleep(2000);
+			
+			// By Clicking Submit the Selection Using Key Event
 			r.keyPress(KeyEvent.VK_ENTER);
 			r.keyRelease(KeyEvent.VK_ENTER);
 			Thread.sleep(2000);
+			
+			// Click on Accredited filter to apply on given Selection
 			WebElement accr = driver.findElement(By.xpath("(//div [@class = 'c-filter__select--checkbox u-d-inlineblock u-valign--middle u-pos-rel'])[2]"));
 			accr.click();
 			Thread.sleep(2000);
+			
+			// Click on Drop Down Icon
 			WebElement allfltr = driver.findElement(By.xpath("//span[@class = 'u-spacer--right-thin']"));
 			allfltr.click();
 			Thread.sleep(2000);
+			
+			// Click On 24X7 Pharmacy Check box Option
 			WebElement phrm = driver.findElement(By.xpath("(//div [@class = 'c-filter__select--checkbox u-d-inlineblock u-valign--middle u-pos-rel'])[6]"));
 			phrm.click();
 			Thread.sleep(2000);
@@ -69,54 +96,81 @@ public class Practo extends Read
 			//Hospital 1
 			
 			WebElement h1 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[3]/div/div[1]/div[1]/div/div[2]/div/a/h2"));
-			System.out.println(h1.getText());
+			String ps = (h1.getText());
+			
 			WebElement info1 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[3]/div/div[1]/div[2]/div"));
-			System.out.println(info1.getText());
-			System.out.println("=====================================================================================================");
+			
+			String ps1 = (info1.getText());
+			
 	
 			// Hospital 2
 			
 			WebElement h2 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[4]/div/div[1]/div[1]/div/div[2]/div/a/h2"));
-			System.out.println(h2.getText());
-			String ps = (h2.getText());
+			
+			String ps2 = (h2.getText());
 			WebElement info2 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[4]/div/div[1]/div[2]"));
-			System.out.println(info2.getText());
-			String ps1 =(info2.getText());
-			System.out.println("=====================================================================================================");
+			
+			String ps3 =(info2.getText());
+			
 						
 			// Hospital 3
 						
 			WebElement h3 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[5]/div/div[1]/div[1]/div/div[2]/div/a/h2"));
-			System.out.println(h3.getText());
+			
+			String ps4 =(h3.getText());
 			WebElement info3 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[5]/div/div[1]/div[2]"));
-			System.out.println(info3.getText());	
-			System.out.println("=====================================================================================================");						
+			
+			String ps5 =(info3.getText());
+									
 						
 						
 			// Hospital 4
 						
 			WebElement h4 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[2]/div/div[1]/div[1]/div/div[2]/div/a/h2"));
-			System.out.println(h4.getText());
+			
+			String ps6 =(h4.getText());
 			WebElement info4 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[2]/div/div[1]/div[2]"));
-			System.out.println(info4.getText());	
-			System.out.println("=====================================================================================================");
+			
+			String ps7 =(info4.getText());
+			
 						
 
 			// Hospital 5
 						
 			WebElement h5 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[8]/div/div[1]/div[1]/div/div[2]/div/a/h2"));
-			System.out.println(h5.getText());
+			
+			String ps8 =(h5.getText());
 			WebElement info5 = driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[2]/div[1]/div/div[3]/div[8]/div/div[1]/div[2]"));
-			System.out.println(info5.getText());	
-			System.out.println("=====================================================================================================");
 			
-			File fs = new File("C:\\Users\\91959\\eclipse-workspace\\MiniProject\\txtfolder\\write.txt");
-			FileUtils.write(fs, ps);
-			FileUtils.write(fs, ps1);
+			String ps9 =(info5.getText());
 			
+			// Making List of Hospital Using Set Function
+			Set<String> se = new LinkedHashSet<String>();
 			
+			se.add(ps);
+			se.add(ps1);
+			se.add(ps2);
+			se.add(ps3);
+			se.add(ps4);
+			se.add(ps5);
+			se.add(ps6);
+			se.add(ps7);
+			se.add(ps8);
+			se.add(ps9);
 			
+			System.out.println("____________________________________________________________________");
+			
+			se.forEach(t -> System.out.println(t));
+			
+			System.out.println("____________________________________________________________________");
+			for(int l=0;l<=2;l++)
+			{
+			File fe = new File("C:\\\\\\\\Users\\\\\\\\91959\\\\\\\\eclipse-workspace\\\\\\\\MiniProject\\\\\\\\txtFolder\\\\\\\\Cities.txt");
+			FileUtils.write(fe, se.toString());
+			}
+
 		}
+		driver.close();
 	
 	}
 
